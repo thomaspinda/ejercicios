ceff71303b20168e218dbc34bac2e7eaa563920f
Thomas Pinda <90642090+thomaspinda@users.noreply.com>
2024-04-24 09:31:26 -0400

    python

---
+++ Python/XD.py
@@ -0,0 +1,17 @@
+from turtle import *
+bgcolor('black')
+speed(10)
+pencolor("red")
+left(45)
+
+for i in range(20):
+    forward(20)
+    left(10)
+right(135)
+for i in range(20):
+    forward(20)
+    left(10)
+forward(280)
+left(90)
+forward(285)
+mainloop()
