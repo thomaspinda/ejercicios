6f90774d3897bdabb4f9d972ce79ee753b2a7d8f
Thomas Pinda <90642090+thomaspinda@users.noreply.com>
2024-04-25 21:00:56 -0400

    React

---
+++ React/testimonios-freecodecamp/src/componentes/Testimonio.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../hojas-de-estilo/Testimonio.css'
+function Testimonio(props){
+  return (
+    <div className='contenedor-testimonio'>
+      <img 
+        className='imagen-testimonio'
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt='Foto de Emma'/>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
